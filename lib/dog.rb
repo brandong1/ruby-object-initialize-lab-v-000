4e8483a1850a6baa53bcc6a2b3e3288039f810c8
Brandon Green <brandon@brandonleeproductions.com>
2018-05-15 02:36:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,22 @@
-class Dog
-    def initialize(name, breed="Mutt")
-        @breed = "Mutt"
-        @name = name
-    end
-  def initialize(name)
+class Dog 
+  def initialize(name, breed="Mut")
+    @name=name
+    @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def name=(name)
     @name = name
   end
-  
-   def breed=(breed) 
-    @breed = breed
-   end
-  def name=(name)
-    @name = name 
-  end
-  
+
   def name
     @name
   end
-  
-   def breed
-    @breed
-   end
 end
-
